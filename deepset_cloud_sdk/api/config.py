1a7f62d6d85f59818f4892090a23fd0297bad8d9
Kristof Herrmann <kristof.herrmann@rwth-aachen.de>
2023-05-10 16:37:38 +0200

    fix: add dotenv to load env vars

--- deepset_cloud_sdk/api/config.py
+++ deepset_cloud_sdk/api/config.py
@@ -3,13 +3,18 @@
 import os
 from dataclasses import dataclass
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 WORKSPACE = os.getenv("WORKSPACE", "default")
-API_URL = os.getenv("api_url", "https://api.cloud.deepset.ai/")
+API_URL = os.getenv("API_URL", "https://api.cloud.deepset.ai/")
+API_KEY = os.getenv("API_KEY", "")
 
 
 @dataclass
 class CommonConfig:
     """Common config for connecting to Deepset Cloud API."""
 
-    api_key: str
+    api_key: str = API_KEY
     api_url: str = API_URL
