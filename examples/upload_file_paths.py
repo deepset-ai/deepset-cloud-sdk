a23c4d4cd36751def602824ac2386884e62c51c1
Kristof Herrmann <kristof.herrmann@rwth-aachen.de>
2023-05-12 00:27:39 +0200

    feat: example upload

--- examples/upload_file_paths.py
+++ examples/upload_file_paths.py
@@ -1,4 +1,5 @@
 from pathlib import Path
-from deepset_cloud_sdk.workflows.async.files import upload_file_paths
+
+from deepset_cloud_sdk.workflows.sync_client.files import upload_file_paths
 
 upload_file_paths(file_paths=[Path("./tmp/my-file")])
