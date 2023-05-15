cc604c76fd21f29ddb4e116c99d5afb403f6da0a
Kristof Herrmann <kristof.herrmann@rwth-aachen.de>
2023-05-15 12:40:18 +0200

    feat: typer cli for uploading files

---
+++ deepset_cloud_sdk/cli.py
@@ -0,0 +1,24 @@
+import typer
+
+from deepset_cloud_sdk.workflows.sync_client.files import (
+    upload_file_paths,
+    upload_folder,
+)
+
+cli_app = typer.Typer()
+
+# cli commands
+cli_app.command()(upload_file_paths)
+cli_app.command()(upload_folder)
+
+
+def run_packaged() -> None:
+    """Run the packaged CLI app.
+
+    This is the entrypoint for the package to enable running the CLI app using typer.
+    """
+    cli_app()
+
+
+if __name__ == "__main__":
+    cli_app()
