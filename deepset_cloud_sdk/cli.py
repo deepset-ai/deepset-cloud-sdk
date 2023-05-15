c06034ef93c6e7303c4bdbbffaef06f7d2159207
Kristof Herrmann <kristof.herrmann@rwth-aachen.de>
2023-05-15 14:51:26 +0200

    feat: deepset-cloud-cli login

--- deepset_cloud_sdk/cli.py
+++ deepset_cloud_sdk/cli.py
@@ -1,6 +1,9 @@
 """CLI app for the deepset cloud SDK."""
+import os
+
 import typer
 
+from deepset_cloud_sdk.api.config import ENV_FILE_PATH
 from deepset_cloud_sdk.workflows.sync_client.files import (
     upload_file_paths,
     upload_folder,
@@ -11,6 +14,25 @@ cli_app = typer.Typer()
 # cli commands
 cli_app.command()(upload_file_paths)
 cli_app.command()(upload_folder)
+
+
+@cli_app.command()
+def login() -> None:
+    """Login to the deepset cloud."""
+    typer.echo("Login to the deepset cloud")
+    passed_api_key = typer.prompt("Your API_KEY", hide_input=True)
+    passed_api_url = typer.prompt("Your API_URL", default="https://api.cloud.deepset.ai/")
+    passed_default_workspace_name = typer.prompt("Your DEFAULT_WORKSPACE_NAME", default="default")
+
+    env_content = (
+        f"API_KEY={passed_api_key}\nAPI_URL={passed_api_url}\nDEFAULT_WORKSPACE_NAME={passed_default_workspace_name}"
+    )
+
+    os.makedirs(os.path.dirname(ENV_FILE_PATH), exist_ok=True)
+    with open(ENV_FILE_PATH, "w") as env_file:
+        env_file.write(env_content)
+
+    typer.echo(f"{ENV_FILE_PATH} created successfully!")
 
 
 def run_packaged() -> None:
---
+++ tests/unit/utils/test_load_configuration.py
@@ -0,0 +1,18 @@
+import os
+from unittest.mock import Mock, patch
+
+from deepset_cloud_sdk.api.config import ENV_FILE_PATH, load_environment
+
+
+def test_load_environment_from_local_env() -> None:
+    current_cwd = os.getcwd()
+    with patch("deepset_cloud_sdk.api.config.os") as mocked_os:
+        mocked_os.path.join.return_value = current_cwd + "/tests/data/.fake-env"
+        mocked_os.path.isfile.return_value = True
+        assert load_environment()
+
+
+@patch("deepset_cloud_sdk.api.config.load_dotenv")
+def test_load_environment_with_login_credentials(mocked_load_dotenv: Mock) -> None:
+    assert load_environment()
+    mocked_load_dotenv.assert_called_once_with(ENV_FILE_PATH)
