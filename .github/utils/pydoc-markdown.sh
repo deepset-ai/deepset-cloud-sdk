76a9647dee92bd649b4aed605cdbe38afd5b74af
Kristof Herrmann <kristof.herrmann@rwth-aachen.de>
2023-06-13 09:39:24 +0200

    feat: add pydoc action for readme

---
+++ .github/utils/pydoc-markdown.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e   # Fails on any error in the following loop
+export PYTHONPATH=$PWD/docs/pydoc # Make the renderers available to pydoc
+cd docs/pydoc
+rm -rf temp && mkdir temp
+cd temp
+for file in ../config/* ; do
+    echo "Converting $file..."
+    pydoc-markdown "$file"
+done
