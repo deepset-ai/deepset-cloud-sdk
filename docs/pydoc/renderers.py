532be583b33edd150e52685e3d96e82a0d9e5814
Kristof Herrmann <kristof.herrmann@rwth-aachen.de>
2023-07-03 15:07:16 +0200

    fix: site url

--- docs/pydoc/renderers.py
+++ docs/pydoc/renderers.py
@@ -4,17 +4,14 @@ import io
 import os
 import sys
 import typing as t
-import warnings
 
 import docspec
-import requests
 from pydoc_markdown.contrib.renderers.markdown import MarkdownRenderer
 from pydoc_markdown.interfaces import Context, Renderer
 
 README_FRONTMATTER = """---
 title: {title}
 excerpt: {excerpt}
-category: {category}
 slug: {slug}
 order: {order}
 hidden: false
@@ -46,7 +43,6 @@ class ReadmeRenderer(Renderer):
     def init(self, context: Context) -> None:
         self.markdown.init(context)
         version = self._doc_version()
-        self.categories = self._readme_categories(version)
 
     def _doc_version(self) -> str:
         """
@@ -60,39 +56,6 @@ class ReadmeRenderer(Renderer):
         # within the same documentation page as the deepset Cloud Docs.
         return "v1.0"
 
-    def _readme_categories(self, version: str) -> t.Dict[str, str]:
-        """
-        Fetch the categories of the given version from Readme.io.
-        README_API_KEY env var must be set to correctly get the categories.
-        Returns dictionary containing all the categories slugs and their ids.
-        """
-        README_API_KEY = os.getenv("README_API_KEY")
-        if not README_API_KEY:
-            warnings.warn("README_API_KEY env var is not set, using a placeholder category ID")
-            return {"open-source-sdk-methods": "ID"}
-
-        token = base64.b64encode(f"{README_API_KEY}:".encode()).decode()
-        headers = {"authorization": f"Basic {token}", "x-readme-version": version}
-
-        page = 1
-        results = {}
-
-        while True:
-            res = requests.get(
-                "https://dash.readme.com/api/v1/categories", params={"page": page}, headers=headers, timeout=60
-            )
-            if not res.ok:
-                sys.exit("Error requesting categories")
-
-            data = res.json()
-            if not data:
-                break
-
-            results.update({c["slug"]: c["id"] for c in data})
-            page += 1
-
-        return results
-
     def render(self, modules: t.List[docspec.Module]) -> None:
         if self.markdown.filename is None:
             sys.stdout.write(self._frontmatter())
@@ -105,7 +68,6 @@ class ReadmeRenderer(Renderer):
     def _frontmatter(self) -> str:
         return README_FRONTMATTER.format(
             title=self.title,
-            category=self.categories[self.category_slug],
             excerpt=self.excerpt,
             slug=self.slug,
             order=self.order,
