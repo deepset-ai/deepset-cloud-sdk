bb79d2e095e0b0fc7cd41516902fe1ba21937e26
Kristof Herrmann <kristof.herrmann@rwth-aachen.de>
2023-09-05 10:01:47 +0200

    fix

--- tests/integration/service/test_integration_files_service.py
+++ tests/integration/service/test_integration_files_service.py
@@ -27,8 +27,8 @@ class TestUploadsFileService:
                 write_mode=WriteMode.KEEP,
                 timeout_s=timeout,
             )
-            assert result.total_files == 10
-            assert result.successful_upload_count == 10
+            assert result.total_files == 20
+            assert result.successful_upload_count == 20
             assert result.failed_upload_count == 0
             assert len(result.failed) == 0
 
@@ -48,8 +48,8 @@ class TestUploadsFileService:
                 write_mode=WriteMode.KEEP,
                 timeout_s=120,
             )
-            assert result.total_files == 5
-            assert result.successful_upload_count == 5
+            assert result.total_files == 10
+            assert result.successful_upload_count == 10
             assert result.failed_upload_count == 0
             assert len(result.failed) == 0
 
