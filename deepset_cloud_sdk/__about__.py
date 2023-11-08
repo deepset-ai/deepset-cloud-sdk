e056609e835b3f2bcb82698c91ae59422addb502
Kristof Herrmann <kristof.herrmann@rwth-aachen.de>
2023-11-08 16:38:31 +0100

    fix: version

--- deepset_cloud_sdk/__about__.py
+++ deepset_cloud_sdk/__about__.py
@@ -1,4 +1,4 @@
 """This file defines the package version."""
 # Will be automatically overridden during the release process
 # It's okay if this is outdated in the repo. We will use the tag from the release as the version.
-__version__ = "0.0.34"
+__version__ = "0.0.35"
