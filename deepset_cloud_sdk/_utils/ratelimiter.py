530d201c2ab095c75bab5c4390627c94aab12e26
rjanjua <rohan.janjua@gmail.com>
2024-03-12 08:30:21 +0000

    refactor and unit tests

--- deepset_cloud_sdk/_utils/ratelimiter.py
+++ deepset_cloud_sdk/_utils/ratelimiter.py
@@ -17,18 +17,17 @@ class RateLimiter:
     This class is not thread-safe.
     """
 
-    RATE = 500  # 500 requests per second, AWS limit is 3500 PUT/POST/DELETE requests a second
-    MAX_TOKENS = 500
-
     def __init__(self, client: ClientSession):
         """
         Create RateLimiter Object.
 
         :param client: Instance of an aiohttp.ClientSession.
         """
-        self.client = client
-        self.tokens = RateLimiter.MAX_TOKENS
-        self.updated_at = time.monotonic()
+        self._client = client
+        self._rate = 500  # 500 requests per second, AWS limit is 3500 PUT/POST/DELETE requests a second
+        self._max_tokens = 500
+        self._tokens = 0
+        self._updated_at = time.monotonic()
 
     async def post(self, *args: Any, **kwargs: Any) -> aiohttp.client._RequestContextManager:
         """
@@ -37,22 +36,22 @@ class RateLimiter:
         :params *args: the arguments to be passed to the ClientSession
         :param **kwargs: the keyword arguments to pass to the ClientSession
         """
-        await self.wait_for_token()
-        return self.client.post(*args, **kwargs)
+        await self._wait_for_token()
+        return self._client.post(*args, **kwargs)
 
-    async def wait_for_token(self) -> None:
+    async def _wait_for_token(self) -> None:
         """Wait until a token becomes available."""
-        while self.tokens < 1:
-            self.add_new_tokens()
+        while self._tokens < 1:
+            self._add_new_tokens()
             await asyncio.sleep(0.1)
-        self.tokens -= 1
+        self._tokens -= 1
 
-    def add_new_tokens(self) -> None:
+    def _add_new_tokens(self) -> None:
         """Add additional tokens based on the time passed and the defined rate."""
         now = time.monotonic()
-        time_since_update = now - self.updated_at
-        new_tokens = int(time_since_update * self.RATE)
+        time_since_update = now - self._updated_at
+        new_tokens = int(time_since_update * self._rate)
 
-        if self.tokens + new_tokens >= 1:
-            self.tokens = min(self.tokens + new_tokens, self.MAX_TOKENS)
-            self.updated_at = now
+        if self._tokens + new_tokens >= 1:
+            self._tokens = min(self._tokens + new_tokens, self._max_tokens)
+            self._updated_at = now
